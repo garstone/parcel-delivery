d34b40ff9b814ba97cfcaebaf84eb59923cdf420
kamenevIA <kamenevia@spvb.ru>
2022-09-04 17:04:57 +0300

    ordercontroller tests

--- order-ms/src/test/java/kamenev/delivery/orderservice/controller/OrderControllerTest.java
+++ order-ms/src/test/java/kamenev/delivery/orderservice/controller/OrderControllerTest.java
@@ -1,12 +1,16 @@
 package kamenev.delivery.orderservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kamenev.delivery.orderservice.dto.OrderDetails;
 import kamenev.delivery.orderservice.errors.ErrorResponse;
+import kamenev.delivery.orderservice.model.ChangeDestinationRequest;
 import kamenev.delivery.orderservice.model.OrderCreateRequest;
 import kamenev.delivery.orderservice.service.OrderService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,36 +20,41 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest(classes = OrderController.class)
-//@AutoConfigureMockMvc
-//@EnableWebMvc
 @ContextConfiguration
 @WebMvcTest(controllers = OrderController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class OrderControllerTest {
 
     @Autowired
     private ObjectMapper mapper;
 
-    @Autowired
-    private OrderController orderController;
-
     @MockBean
     private OrderService orderService;
 
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    private final static String CHANGE_DESTINATION = "/api/orders/change-destination/";
+    private final static String CREATE = "/api/orders/create/";
+    private final static String CANCEL = "/api/orders/cancel/";
+
+    @BeforeAll
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
     void create_Ok() throws Exception{
@@ -54,7 +63,7 @@ class OrderControllerTest {
         orderDetails.setUserName("Vladimir");
         when(orderService.create(eq(req))).thenReturn(orderDetails);
 
-        mockMvc.perform(put("/api/orders/create")
+        mockMvc.perform(put(CREATE)
                         .content(mapper.writeValueAsString(req))
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("UTF-8"))
@@ -68,7 +77,7 @@ class OrderControllerTest {
         var errResponse = new ErrorResponse("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
         when(orderService.create(req)).thenThrow(new RuntimeException("ERROR"));
 
-        mockMvc.perform(put("/api/orders/create")
+        mockMvc.perform(put(CREATE)
                         .content(mapper.writeValueAsString(req))
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("UTF-8"))
@@ -80,9 +89,7 @@ class OrderControllerTest {
     void create_InvalidRequest() throws Exception {
         var req = new OrderCreateRequest(null, "1", "", "", "");
 
-        var contextMockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-
-        contextMockMvc.perform(put("/api/orders/create")
+        mockMvc.perform(put(CREATE)
                         .content(mapper.writeValueAsString(req))
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("UTF-8"))
@@ -90,18 +97,64 @@ class OrderControllerTest {
     }
 
     @Test
-    void changeDestination_Ok() {
+    void changeDestination_Ok() throws Exception {
+        var req = new ChangeDestinationRequest(UUID.randomUUID(), "Sochi, Mansion");
+        var orderDetails = new OrderDetails();
+        orderDetails.setUserName("Vladimir");
+        when(orderService.changeDestination(any(), eq("Sochi, Mansion"))).thenReturn(orderDetails);
 
+        mockMvc.perform(patch(CHANGE_DESTINATION)
+                        .content(mapper.writeValueAsString(req))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(mapper.writeValueAsString(orderDetails)));
     }
 
     @Test
-    void changeDestination_Throws() {
+    void changeDestination_ThrowsResponseStatusException() throws Exception {
+        var req = new ChangeDestinationRequest(UUID.randomUUID(), "Sochi, Mansion");
+        var ex = new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Order with id=%s does not exist", "test"));
+        when(orderService.changeDestination(any(), eq("Sochi, Mansion"))).thenThrow(ex);
 
+        mockMvc.perform(patch(CHANGE_DESTINATION)
+                        .content(mapper.writeValueAsString(req))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void cancel() {
+    void changeDestination_InvalidRequest() throws Exception {
+        var req = new ChangeDestinationRequest(null, "");
+
+        mockMvc.perform(patch(CHANGE_DESTINATION)
+                        .content(mapper.writeValueAsString(req))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void cancel_Ok() throws Exception {
+        doNothing().when(orderService).cancel(any());
+
+        var res = mockMvc.perform(post(CANCEL + UUID.randomUUID())
+                        .contentType(MediaType.ALL)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void cancel_InvalidRequest() throws Exception {
+        var res = mockMvc.perform(post(CANCEL + "abc")
+                        .content("{'a': 'b'}")
+                        .contentType(MediaType.ALL)
+                        .characterEncoding("UTF-8"))
+                .andExpect(status().isBadRequest());
+    }
+
+    // todo too lazy to test web layer more... maybe later
 
     @Test
     void getDetails() {
